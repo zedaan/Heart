ccae911d50631baba158d3e09330654e08dbf9ca
zedaan <zedaanrumi@gmail.com>
2018-04-05 15:56:16 +0500

    Adding Semantic CSS

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -11,6 +11,7 @@ import { Helmet } from 'react-helmet';
 import styled from 'styled-components';
 import { Switch, Route } from 'react-router-dom';
 
+import 'semantic-ui-css/semantic.css';
 import Promo from 'containers/Promo/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
 
